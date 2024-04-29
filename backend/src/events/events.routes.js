71108d0ede753e8fd8032a34c41ebfd74a2b9ed1
Luciano Maquin <134962683+lmaquin2019274@users.noreply.github.com>
2024-04-29 11:01:21 -0600

    Delete backend directory

--- backend/src/events/events.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { check } from "express-validator";
-import {  } from "./events.controller.js";
-const router = Router()
-
-export default router;
--- backend/src/hotels/hotels.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { check } from "express-validator";
-import {  } from "./hotels.controller.js";
-const router = Router()
-
-export default router;
